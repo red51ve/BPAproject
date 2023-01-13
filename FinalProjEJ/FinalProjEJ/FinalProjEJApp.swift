8ba9e4fe1dbfdf9826ba4008970e7c529efb3c61
red51ve <113051930+red51ve@users.noreply.github.com>
2023-01-13 12:23:37 -0600

    Add files via upload

---
+++ FinalProjEJ/FinalProjEJ/FinalProjEJApp.swift
@@ -0,0 +1,19 @@
+//
+//  FinalProjEJApp.swift
+//  FinalProjEJ
+//
+//  Created by PM Student on 12/6/22.
+//
+
+import SwiftUI
+
+
+
+@main
+struct FinalProjEJApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
